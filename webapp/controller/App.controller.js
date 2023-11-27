dd731d75643903a9d0360b73da4c2e0c9b50f0d3
bas01 clouddnagmbhgmail.onmicrosoft.com <bas01@clouddnagmbhgmail.onmicrosoft.com>
2023-11-27 14:52:30 +0000

    initial commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("at.clouddna.training01.zalexapp1.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"atclouddnatraining01/zalexapp1/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
