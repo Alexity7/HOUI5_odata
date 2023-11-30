95ced2a0619e281f612c26b72cd2fc92c12bbdf7
bas01 clouddnagmbhgmail.onmicrosoft.com <bas01@clouddnagmbhgmail.onmicrosoft.com>
2023-11-30 10:43:41 +0000

    aufgabe9

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "at/clouddna/training01/zalexapp1/data/formatter/Formatter"
+    "at/clouddna/training01/zalexapp1/data/formatter/Formatter",
+    "sap/m/MessageBox"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, Formatter) {
+    function (Controller, Formatter, MessageBox) {
         "use strict";
 
         return Controller.extend("at.clouddna.training01.zalexapp1.controller.Main", {
@@ -13,7 +14,7 @@ sap.ui.define([
             formatter: Formatter,
 
             onInit: function () {
-
+                
             },
 
             dateFormatter: function (sDate) {
@@ -21,16 +22,30 @@ sap.ui.define([
                 return dateObj.getDate() + "." + (dateObj.getMonth() + 1) + "." + dateObj.getFullYear();
             },
 
-            onListItemClicked: function(oEvent) {
+            onListItemClicked: function (oEvent) {
                 const sPath = oEvent.getSource().getBindingContext().getPath();
-            
+
                 this.getOwnerComponent().getRouter().navTo("RouteCustomer", {
                     path: encodeURIComponent(sPath)
                 }, false);
             },
-            onCreatePressed: function() {
+            onCreatePressed: function () {
                 this.getOwnerComponent().getRouter().navTo("CreateCustomer", null, false);
+            },
+            onDeletePressed: function (oEvent) {
+
+                let sSelectedPath = oEvent.getParameter("listItem").getBindingContext().getPath();
+
+                this.getView().getModel().remove(sSelectedPath, {
+                    success: function (oData, response) {
+                        this.getView().getModel().refresh();
+                        MessageBox.success("Erfolgreich gelöscht");
+                    }.bind(this),
+                    error: (oError) => {
+                        console.error(oError);
+                    }
+                });
             }
         }
-);
+        );
     });
